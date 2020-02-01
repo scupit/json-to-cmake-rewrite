f3d128f2a4ae5aede9c67b7ecb0ddea552d18b0d
Skylar Cupit <Skylarcupit@gmail.com>
2020-02-01 00:57:59 -0600

    Refactored BuildData a bit

--- BuildData.py
+++ BuildData.py
@@ -1,7 +1,9 @@
 import json
-from Globals import JSON_FILE_NAME
-import Tags
+
 import FileHelper
+import Globals
+import Logger
+import Tags
 
 from ImportedLibrary import ImportedLibrary
 from OutputItem import OutputItem
@@ -22,16 +24,20 @@ class BuildData:
   defaultBuildTarget = None
 
   def __init__(self):
-    with open(FileHelper.getAbsolutePath(JSON_FILE_NAME)) as jsonFile:
+    with open(FileHelper.getAbsolutePath(Globals.JSON_FILE_NAME)) as jsonFile:
       jsonData = json.load(jsonFile)
 
       self.loadOutputs(jsonData)
       self.loadImportedLibs(jsonData)
 
   def loadOutputs(self, jsonData):
+    if not Tags.OUTPUT in jsonData or len(jsonData[Tags.OUTPUT]) == 0:
+      Logger.logIssueThenQuit(f"Must define at least one {Tags.OUTPUT}")
+
     for name, outputData in jsonData[Tags.OUTPUT].items():
       self.outputs.append(OutputItem(name, outputData))
 
   def loadImportedLibs(self, jsonData):
-    for name, importedLibData in jsonData[Tags.IMPORTED_LIBRARIES].items():
-      self.importedLibs.append(ImportedLibrary(name, importedLibData))
+    if Tags.IMPORTED_LIBRARIES in jsonData:
+      for name, importedLibData in jsonData[Tags.IMPORTED_LIBRARIES].items():
+        self.importedLibs.append(ImportedLibrary(name, importedLibData))
